549d6ed5a0f36a01c66d92ecf11849e40c111eef
Aly Filekesh <alifilekesh@gmail.com>
2022-06-06 14:15:37 +0430

    fix: remove import react

--- apps/client/src/modules/common/components/Spinner/Spinner.tsx
+++ apps/client/src/modules/common/components/Spinner/Spinner.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-
-export const Spinner = () => {
+const Spinner = () => {
   return (
     <div data-testid="loading" className="flex justify-center items-center mt-16">
       <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
     </div>
   );
 };
+
+export default Spinner
