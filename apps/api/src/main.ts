6fca84bda2f642a8f24e429c291e6c19cb1c3d9a
Aly Filekesh <alifilekesh@gmail.com>
2022-06-06 13:14:23 +0430

    feat: add search user dto

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,20 +3,29 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
-}
-
-bootstrap();
+ import { Logger, ValidationPipe } from '@nestjs/common';
+ import { NestFactory } from '@nestjs/core';
+ 
+ import { AppModule } from './app.module';
+ 
+ async function bootstrap() {
+   const app = await NestFactory.create(AppModule);
+   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+   const globalPrefix = 'api';
+   app.setGlobalPrefix(globalPrefix);
+   const options = {
+     origin: '*',
+     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+     preflightContinue: false,
+     credentials: true,
+   };
+   app.enableCors(options);
+   const port = process.env.PORT || 3333;
+   await app.listen(port);
+   Logger.log(
+     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+   );
+ }
+ 
+ bootstrap();
+ 
