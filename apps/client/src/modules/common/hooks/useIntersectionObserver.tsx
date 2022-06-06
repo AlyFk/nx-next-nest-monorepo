1ff7bb8a36f0018bfab6fe3f3dccddbc48176c4a
Aly Filekesh <alifilekesh@gmail.com>
2022-06-06 14:03:47 +0430

    feat: add intersection observer hook

---
+++ apps/client/src/modules/common/hooks/useIntersectionObserver.tsx
@@ -0,0 +1,29 @@
+import { useEffect, RefObject } from 'react';
+
+const useIntersectionObserver = (
+  target: RefObject<HTMLElement>,
+  callback: () => void,
+  options?: IntersectionObserverInit
+) => {
+  useEffect(() => {
+    const currentObserver = new IntersectionObserver((entries) => {
+      entries.forEach(async (entry) => {
+        if (entry.isIntersecting) {
+          callback();
+        }
+      });
+    }, options);
+    const element = target.current;
+    if (element) {
+      currentObserver.observe(element);
+    }
+
+    return () => {
+      if (element) {
+        currentObserver.unobserve(element);
+      }
+    };
+  }, [callback, options, target]);
+};
+
+export default useIntersectionObserver
