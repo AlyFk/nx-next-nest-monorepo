2c12e54053601b374161df890176054beae45ccb
Aly Filekesh <alifilekesh@gmail.com>
2022-06-07 12:28:20 +0430

    feat: add toast for error handling

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import AppProvider from 'app/providers';
 import { AppProps } from 'next/app';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import '../styles.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AppProvider pageProps={pageProps}>
       <Component {...pageProps} />
+      <ToastContainer />
     </AppProvider>
   );
 }
--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 
@@ -6,33 +6,50 @@ import { UserCard, SearchBar } from 'modules/users/components';
 import { useGetUsers, prefetchUsers } from 'modules/users/queries/getUsers';
 import useIntersectionObserver from 'modules/common/hooks/useIntersectionObserver';
 import { Spinner } from 'modules/common/components';
+import { toast } from 'react-toastify';
+
+const limit = 12;
+const toastId = 'users-page-toast';
 
 const Home = () => {
   const router = useRouter();
+  const { username } = router.query;
 
   const handleSearch = (value) =>
     router.push({ pathname: '/', query: { username: value } }, undefined, {
       shallow: true,
     });
 
-  const { username } = router.query;
-
-
   const target = useRef<HTMLDivElement>(null);
   useIntersectionObserver(target, () => fetchNextPage(), {
     threshold: 0.8,
   });
 
+  const errorNotify = (message) => {
+    toast.error(`Something went wrong: ${message}`, {
+      toastId,
+    });
+  };
+
   const { data, isFetchingNextPage, fetchNextPage, isLoading, isSuccess } =
     useGetUsers(
-      { limit: 12, username: (username as string) || '' },
+      { limit, username: (username as string) || '' },
       {
         query: {
+          retry: (failureCount, error) => {
+            errorNotify(error.message);
+            if (failureCount < 4) {
+              return true;
+            }
+            return false;
+          },
+          onError: (error) => {
+            errorNotify(error.message);
+          },
           staleTime: 30 * 1000,
         },
       }
     );
-
   return (
     <div className="container mx-auto">
       <SearchBar onSubmit={handleSearch} />
@@ -60,7 +77,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const { username } = query;
   const usersProps = await prefetchUsers({
     username: (username as string) || '',
-    limit: 12,
+    limit,
     page: 1,
   });
 
